ac8a917caa147773e34f1609a9ece64a04960fdb
Thomas LEBLOND <thomas.leblond@epita.fr>
2022-09-25 17:09:21 +0200

    Add mandatory return codes

--- find_motive/find_motive.cpp
+++ find_motive/find_motive.cpp
@@ -6,11 +6,11 @@ int find_motive(std::string path, std::string motive)
 
     std::ifstream File(path);
     std::string word;
-    int occurance = 0;
-    int found;
+    int occurance = -1;
  
     if (File.is_open()) 
     {
+        occurance = 0;
         while (File >> word) 
         {
             if (word.find(motive) != std::string::npos) {
--- find_motive/main.cpp
+++ find_motive/main.cpp
@@ -2,29 +2,21 @@
 
 int main(int argc, char* argv[])
 {
-    std::string motive;
-    std::string path;
-
     int ret_val = 0;
-    int occurance = 0;
+    int occurance = 0; 
 
-    if (argc < 3) 
+    occurance = find_motive(argv[1], argv[2]);
+
+    if (occurance != -1)
     {
         ret_val = 1;
-    } 
-    else
-    {
-        path = argv[1];
-        motive = argv[2];
-    }
 
-    if (ret_val == 0) 
-    {
-        occurance = find_motive(path, motive);
-        if (occurance != 0)
+        if (occurance > 0)
         {
-            std::cout<<"The file "<< path <<" contains "<< occurance <<" words containing the motive "<< motive <<std::endl;
+            ret_val = 2;
         }
+        
+        std::cout<<"The file "<< argv[1] <<" contains "<< occurance <<" words containing the motive "<< argv[2] <<std::endl;
     }
 
     return ret_val;
