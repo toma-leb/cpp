3aed780f92cd91874bfcddb1a20bcf757e4eaac8
Thomas LEBLOND <thomas.leblond@epita.fr>
2022-10-18 21:26:13 +0200

    Fix is_prime

--- is_prime_prog/main.cpp
+++ is_prime_prog/main.cpp
@@ -6,13 +6,14 @@ int main()
 {
     std::string input;
     std::string val;
+    std::string::size_type sz;
 
     std::getline(std::cin,input);
     std::stringstream sstream(input);
     
     while (std::getline(sstream, val, ' '))
     {
-        std::cout<<val<<" is a prime: "<<is_prime(stoi(val))<<std::endl;
+        std::cout<<val<<" is a prime: "<<is_prime(stoll(val))<<std::endl;
     }
     
     return 1;
